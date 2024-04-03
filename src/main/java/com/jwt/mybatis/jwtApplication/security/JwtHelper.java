d9e6ee4c17a350f0c2c8c9e0120b25df8c4ecde4
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-03 19:31:59 +0530

    Converted string to SecretKey object

--- src/main/java/com/jwt/mybatis/jwtApplication/security/JwtHelper.java
+++ src/main/java/com/jwt/mybatis/jwtApplication/security/JwtHelper.java
@@ -5,22 +5,24 @@ import com.jwt.mybatis.jwtApplication.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-@PropertySource("classpath:application.properties")
 public class JwtHelper {
 
     public static final long JWT_TOKEN_VALIDITY = 30 * 60; // 30 minutes
 
-    private String secret = "0ddf5597e02d981f8803c4cc11f015a4e52679d706edb29b595d9e466def5bcf95273a3053ab5d97ee893c23e4023b912daefaade316406a33b7685d4d223dfa";
+    @Value("${jwt.secret}")
+    private String secret;
 
     @Autowired
     private UserRepository userRepository;
@@ -33,12 +35,16 @@ public class JwtHelper {
     }
 
     private String doGenerateToken(Map<String, Object> claims, String subject) {
+        byte[] secretBytes = secret.getBytes();
+        SecretKey secretKey = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS512.getJcaName());
+
         return Jwts.builder()
+                .setIssuer("vaibhav")
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
 
